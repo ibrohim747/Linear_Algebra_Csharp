1ba1b3ef5290072e3ac9f0129347c1aeebe91a5b
Ibragim <ibrohim7477454@gmail.com>
2025-02-02 19:18:02 +0300

    Create folder Part_1

---
+++ Part_1/01 Basic Concepts/101_Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Algebra_Csharp.Part_1._01_Basic_Concepts
+{
+    internal class _101_Program
+    {
+    }
+}
