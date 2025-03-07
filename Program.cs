6fbde3b9de86ace4363af894592b6018fd74d4e1
Ibragim <ibrohim7477454@gmail.com>
2025-03-07 22:27:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Linear_Algebra_Csharp.Part_1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace Linear_Algebra_Csharp
     {
         static void Main(string[] args)
         {
+            Quadrant qt1 = new Quadrant();
+            qt1.Finding_part(5, 3); //quadrant I
+            qt1.Finding_part(-2, 4); //quadrant II
+            qt1.Finding_part(-3, -3); //quadrant III
+            qt1.Finding_part(4, -1); //quadrant IV
+            qt1.Finding_part(0, 5); //quadrant V
+
+
+            Linear_equation lq = new Linear_equation();
+            string b = Convert.ToString(lq.Linear_eq());
+
+            Console.WriteLine(b);
+            
+            Console.ReadKey();
         }
     }
 }
