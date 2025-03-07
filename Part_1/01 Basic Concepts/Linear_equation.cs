448c0e3bcbe5b63ca01340c228f12d44df11543c
Ibragim <ibrohim7477454@gmail.com>
2025-03-07 22:25:45 +0300

    Add code in Chapter 1

---
+++ Part_1/01 Basic Concepts/Linear_equation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Linear_Algebra_Csharp.Part_1
+{
+    internal class Linear_equation
+    {
+        private bool IsFloatValid(string value)
+        {
+            return float.TryParse(value, out _);
+        }
+
+        //----------------------------------------------------------------------------------
+        public object Linear_eq()
+        {
+            Console.WriteLine("\n" + "y = m*x + b\n" + "I");
+            char y1 = Console.ReadKey().KeyChar;
+            if (y1 != 'x' && y1 != 'y')
+            {
+                Console.WriteLine("\nError: Enter only 'y' or 'x'.");
+            }
+
+            Console.WriteLine("\nEnter M");
+            string m1 = Console.ReadLine();
+            Console.WriteLine("Enter X");
+            string x1 = Console.ReadLine();
+            Console.WriteLine("Enter B");
+            string b1 = Console.ReadLine();
+
+            //Check if the entered values are numbers - Проверяем, являются ли введённые значения числами
+
+            object m; // Используем object, чтобы хранить либо float, либо string
+            object x;
+            object b;
+
+            if (IsFloatValid(m1))
+            {
+                m = float.Parse(m1); // Преобразуем в float
+            }
+            else
+            {
+                m = m1; // Оставляем строкой
+            }
+
+            if (IsFloatValid(x1))
+            {
+                x = float.Parse(x1); // Преобразуем в float
+            }
+            else
+            {
+                x = x1; // Оставляем строкой
+            }
+
+            if (IsFloatValid(b1))
+            {
+                b = float.Parse(b1); // Преобразуем в float
+            }
+            else
+            {
+                b = b1; // Оставляем строкой
+            }
+
+            object result; // Переменная для результата
+            if (m is float mf && x is float xf)
+            {
+                if (b is float bf)
+                {
+                    result = mf * xf + bf;
+                }
+                else
+                {
+                    result = $"{mf} * {xf} + {b}"; // b - строка
+                }
+            }
+            else
+            {
+                result = $"{m} * {x} + {b}"; // m или x - строка
+            }
+            return result;
+        }
+    }
+}
---
+++ Part_1/01 Basic Concepts/Quadrant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Algebra_Csharp.Part_1
+{
+    internal class Quadrant
+    {
+        public void Finding_part(int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("quadrant I");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("quadrant II");
+            }
+            else if (x < 0 && y  < 0)
+            {
+                Console.WriteLine("quadrant III");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("quadrant IV");
+            }
+            else { Console.WriteLine("On an axis"); }
+        }
+    }
+}
